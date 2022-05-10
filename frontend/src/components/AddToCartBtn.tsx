8936b165391528e96c8e94471e8611cb91d0447a
SpiritedAwayCookies <sodagrapefruitfoxtea@protonmail.com>
2022-05-10 11:42:06 -0700

    temp

--- frontend/src/components/AddToCartBtn.tsx
+++ frontend/src/components/AddToCartBtn.tsx
@@ -6,32 +6,60 @@ import { appContext, appSetStateContext } from "../AppState";
 //     id: number
 // }
 
-function AddToCartBtn({ pid,productName, price }) {
+function AddToCartBtn({ pid, productName, price }) {
     const value = useContext(appContext)
     const stState = useContext(appSetStateContext);
+    let found = value.cart.find(ele => ele.pid == pid);
+    console.log("found", found);
+    if (found !== undefined) {
+        var quantity = found?.quantity
+    } else {
 
-      //千万不要用useState, 会不停的重复渲染
-    let quantity = 0;
-    let totalPrice = price * quantity;
+        var quantity: number = 0;
+    }
+
+    //千万不要用useState, 会不停的重复渲染
     const addToCart = (pid) => {
-        quantity += 1;
-       
-        
-        if (stState) {
-            console.log(quantity);
-            stState(stt => {
-                return {
-                    ...stt,
-                    cart: [...stt.cart, {
-                        pid, productName,quantity, totalPrice
-                    }]
-                }
-            }
+        if (quantity === 0) {
+            quantity += 1;
+            let totalPrice = price * quantity;
 
-            )
+            if (stState) {
+                console.log(quantity);
+                stState(stt => {
+                    return {
+                        ...stt,
+                        cart: [...stt.cart, {
+                            pid, productName, quantity, totalPrice
+                        }]
+                    }
+                }
+
+                )
+            }
+            console.log(value.cart);
+        } else {
+            // quantity += 1;
+            // let totalPrice = price * quantity;
+            // if (found !== undefined) {
+            //     found.quantity = quantity;
+            // }
+            // if (stState) {
+            //     console.log(quantity);
+            //     stState(stt => {
+            //         return {
+            //             ...stt,
+            //             cart: [...stt.cart, {
+            //                 pid, productName, quantity, totalPrice
+            //             }]
+            //         }
+            //     }
+
+            //     )
+            // }
+            // console.log(value.cart);
         }
-        console.log(value.cart);
-        
+
     }
     return <>
         <div className="text-center">
