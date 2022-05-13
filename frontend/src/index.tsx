72a7e52b71d4e4eae400b6824370668927a9934b
SpiritedAwayCookies <sodagrapefruitfoxtea@protonmail.com>
2022-05-13 14:06:25 -0700

    save temp

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles.css'
 import 'antd/dist/antd.css';
 import App from './App';
 import { AppStateProvider } from './AppState';
